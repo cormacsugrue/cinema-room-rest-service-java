9783b6e53acacc28eb0326019b29f09f4cf5a032
Cormac <sugrue.cormac@gmail.com>
2025-07-02 17:08:28 +0100

    chore: remove unused imports

--- Cinema Room REST Service with Java/task/src/cinema/Cinema.java
+++ Cinema Room REST Service with Java/task/src/cinema/Cinema.java
@@ -1,7 +1,5 @@
 package cinema;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
--- Cinema Room REST Service with Java/task/src/cinema/CinemaController.java
+++ Cinema Room REST Service with Java/task/src/cinema/CinemaController.java
@@ -3,9 +3,7 @@ package cinema;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 public class CinemaController {
